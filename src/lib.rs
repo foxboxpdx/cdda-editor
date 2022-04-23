cbcfb63a4f0a35ca1b7e9788f51d3fddd9381df6
Fox Box <foxboxpdx@gmail.com>
2022-04-23 02:07:36 -0700

    Major simplifications to savefile parsing and updated for CDDA v0.F-3

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,12 @@ use std::io::{BufReader, BufRead, Error, BufWriter, Write};
 use serde_json::{Value};
 use std::collections::HashMap;
 
-pub mod azul;
-
 // Initialize a CDDASave struct from a file
 impl CDDASave {
     pub fn from_file(fname: &str) -> Result<CDDASave, Error> {
         let f = File::open(fname)?;
         let reader = BufReader::new(f);
-        // Need to skip first line
+        // First line is a comment
         let mut data = String::new();
         let mut lines_iter = reader.lines();
         if let Some(_header) = lines_iter.next() { }
@@ -61,11 +59,12 @@ impl CDDASave {
     }
 }
 
-// Define JSON data structure as a struct
+// Define JSON data structure as best as possible
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct CDDASave {
     pub turn: i64,
     pub calendar_start: i64,
+    pub game_start: i64,
     pub initial_season: i32,
     pub auto_travel_mode: bool,
     pub run_mode: i32,
@@ -76,146 +75,13 @@ pub struct CDDASave {
     pub om_x: i32,
     pub om_y: i32,
     pub grscent: String, // This is some space-separated Vec of i32s?
-    pub active_monsters: Vec<ActiveMonster>,
-    pub stair_monsters: Vec<ActiveMonster>,
-    pub kill_tracker: Kills,
-    pub stats_tracker: Stats,
+    pub typescent: String,
+    pub active_monsters: Value, // Simplify these four into just generic Value types
+    pub stair_monsters: Value,  // as there is no reason to be parsing and manipulating them
+    pub kill_tracker: Value,    // at least for initial versions
+    pub stats_tracker: Value,   // Especially since stats has some horrendous Vec<Vec<HashMap>> data structure
     pub player: Player,
-    pub player_messages: PlayerMessages
-}
-
-// A struct for active monster data
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct ActiveMonster {
-    pub moves: i32,
-    pub pain: i32,
-    pub effects: HashMap<String, HashMap<String, Effect>>,
-    pub values: HashMap<String, String>,
-    pub blocks_left: i32,
-    pub dodges_left: i32,
-    pub num_blocks_bonus: i32,
-    pub num_dodges_bonus: i32,
-    pub armor_bash_bonus: i32,
-    pub armor_cut_bonus: i32,
-    pub speed: i32,
-    pub speed_bonus: i32,
-    pub dodge_bonus: f32,
-    pub block_bonus: i32,
-    pub hit_bonus: f32,
-    pub bash_bonus: i32,
-    pub cut_bonus: i32,
-    pub bash_mult: f32,
-    pub cut_mult: f32,
-    pub melee_quiet: bool,
-    pub grab_resist: i32,
-    pub throw_resist: i32,
-    pub typeid: String,
-    pub unique_name: String,
-    pub posx: i32,
-    pub posy: i32,
-    pub posz: i32,
-    pub wandx: i32,
-    pub wandy: i32,
-    pub wandz: i32,
-    pub wandf: i32,
-    pub hp: i32,
-    pub special_attacks: Value,
-    pub friendly: i32,
-    pub fish_population: i32,
-    pub faction: String,
-    pub mission_id: i32,
-    pub no_extra_death_drops: bool,
-    pub dead: bool,
-    pub anger: i32,
-    pub morale: i32,
-    pub hallucination: bool,
-    pub stairscount: i32,
-    pub tied_item: Value,
-    pub battery_item: Value,
-    pub destination: Vec<i32>,
-    pub ammo: Value,
-    pub underwater: bool,
-    pub upgrades: bool,
-    pub upgrade_time: i32,
-    pub last_updated: i64,
-    pub reproduces: bool,
-    pub baby_timer: Value,
-    pub last_baby: Option<i64>,
-    pub biosignatures: bool,
-    pub biosig_timer: i32,
-    pub last_biosig: Option<i64>,
-    pub summon_time_limit: Value,
-    pub inv: Vec<Inventory>,
-    pub dragged_foe_id: i32,
-    pub mounted_player_id: i32,
-    pub path: Vec<Value>
-}
-
-// A struct for the kill tracker data
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct Kills {
-    pub kills: HashMap<String, i32>,
-    pub npc_kills: Vec<String>
-}
-
-// A struct for the stat tracker data
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct Stats {
-    pub data: DataHash,
-    pub initial_scores: Vec<String>
-}
-
-// A struct for the various datapoints of the stat tracker
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct DataHash {
-    pub administers_mutagen: Option<EventCounts>,
-    pub loses_addiction: Option<EventCounts>,
-    pub gains_addiction: Option<EventCounts>,
-    pub character_takes_damage: Option<EventCounts>,
-    pub triggers_alarm: Option<EventCounts>,
-    pub character_loses_effect: Option<EventCounts>,
-    pub gains_skill_level: Option<EventCounts>,
-    pub seals_hazardous_material_sarcophagus: Option<EventCounts>,
-    pub character_gets_headshot: Option<EventCounts>,
-    pub evolves_mutation: Option<EventCounts>,
-    pub character_gains_effect: Option<EventCounts>,
-    pub game_start: Option<EventCounts>,
-    pub character_triggers_trap: Option<EventCounts>,
-    pub gains_mutation: Option<EventCounts>,
-    pub character_heals_damage: Option<EventCounts>,
-    pub avatar_moves: Option<EventCounts>,
-    pub character_kills_monster: Option<EventCounts>,
-    pub throws_up: Option<EventCounts>,
-    pub installs_cbm: Option<EventCounts>
-}
-
-// EventCounts in the JSON is an array, with each element being
-// a StatEvent + i32 tuple
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct EventCounts {
-    pub event_counts: Vec<(StatEvent, i32)>
-}
-
-// The possible types of events defined in event_counts
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct StatEvent {
-    pub character: Option<Vec<String>>,
-    pub technique: Option<Vec<String>>,
-    pub add_type: Option<Vec<String>>,
-    pub damage: Option<Vec<String>>,
-    pub effect: Option<Vec<String>>,
-    pub new_level: Option<Vec<String>>,
-    pub skill: Option<Vec<String>>,
-    pub from_trait: Option<Vec<String>>,
-    pub to_trait: Option<Vec<String>>,
-    pub avatar_id: Option<Vec<String>>,
-    pub trap: Option<Vec<String>>,
-    #[serde(rename = "trait")]
-    pub _trait: Option<Vec<String>>,
-    pub mount: Option<Vec<String>>,
-    pub killer: Option<Vec<String>>,
-    pub victim_type: Option<Vec<String>>,
-    pub bionic: Option<Vec<String>>
+    pub player_messages: Value, // Simplify also
 }
 
 // A struct for player data
@@ -223,11 +89,9 @@ pub struct StatEvent {
 pub struct Player {
     pub moves: i32,
     pub pain: i32,
-    // Effects is kinda gross. It's a hashmap where the keys are
-    // names of effects, but for some reason there's another hash
-    // between that and the actual Effect part.
-    pub effects: HashMap<String, HashMap<String, Effect>>,
-    pub values: HashMap<String, String>,
+    pub effects: Value, // Simplify for now!
+    pub damage_over_time_map: Value, // Need further research
+    pub values: HashMap<String, String>, // I hope this is all it is ugh
     pub blocks_left: i32,
     pub dodges_left: i32,
     pub num_blocks_bonus: i32,
@@ -244,8 +108,12 @@ pub struct Player {
     pub bash_mult: f32,
     pub cut_mult: f32,
     pub melee_quiet: bool,
-    pub grab_resist: i32,
     pub throw_resist: i32,
+    pub last_updated: i64,
+    pub body: BodyInfo,
+    pub posx: i32,
+    pub posy: i32,
+    pub posz: i32,
     pub str_cur: i32,
     pub str_max: i32,
     pub dex_cur: i32,
@@ -258,117 +126,135 @@ pub struct Player {
     pub dex_bonus: i32,
     pub per_bonus: i32,
     pub int_bonus: i32,
+    pub base_age: i32,
+    pub base_height: i32,
+    pub blood_type: String,
+    pub blood_rh_factor: bool,
+    pub custom_profession: String,
     pub healthy: i32,
     pub healthy_mod: i32,
-    pub healed_24h: Vec<i32>,
     pub thirst: i32,
     pub hunger: i32,
     pub fatigue: i32,
+    pub activity_history: HashMap<String, Value>, // Mostly string/i32 but there's a bool in there
     pub sleep_deprivation: i32,
     pub stored_calories: i64,
     pub radiation: i32,
     pub stamina: i64,
+    pub vitamin_levels: HashMap<String, i32>,
+    pub pkill: i32,
+    pub omt_path: Value, // Need further research
+    pub consumption_history: Value, // A vec of hashmaps
+    pub destination_activity: Value, // These four look to be hashmaps with a single key of 'type'
+    pub activity: Value,             // with a value of ACT_NULL but need further research
+    pub stashed_outbounds_activity: Value,
+    pub stashed_outbounds_backlog: Value,
+    pub backlog: Value, // Need further research
+    pub activity_vehicle_part_index: i32,
+    pub stim: i32,
+    pub type_of_scent: String,
     pub underwater: bool,
     pub oxygen: i32,
     pub traits: Vec<String>,
     pub mutations: HashMap<String, Mutation>,
+    pub magic: Magic,
+    pub martial_arts_data: MartialArts,
     pub my_bionics: Vec<Bionic>,
+    pub move_mode: String,
+    pub morale: Vec<MoraleModifier>,
     pub skills: HashMap<String, Skill>,
-    pub power_level: i32,
+    pub proficiencies: ProficiencyData,
+    pub power_level: String,  // This used to be an i32...
     pub max_power_level: i32,
-    pub posx: i32,
-    pub posy: i32,
-    pub posz: i32,
-    pub stim: i32,
+    pub stomach: GutContents,
+    pub guts: GutContents,
+    pub automoveroute: Vec<Value>, // Need further research
+    pub known_traps: Value, // Yeah just simplify this to generic Value for now. Absolutely MASSIVE though.
     pub last_sleep_check: i64,
-    pub pkill: i32,
-    pub tank_plut: i32,
-    pub reactor_plut: i32,
     pub slow_rad: i32,
     pub scent: i32,
-    pub body_wetness: Vec<i32>,
     pub male: bool,
     pub cash: i64,
-    pub recoil: f32,
+    pub recoil: f32, // For some reason this is parsing as a float instead of an int?
     pub in_vehicle: bool,
     pub id: i32,
-    pub hp_cur: Vec<i32>,
-    pub hp_max: Vec<i32>,
-    pub damage_bandaged: Vec<i32>,
-    pub damage_disinfected: Vec<i32>,
-    pub ma_styles: Vec<String>,
-    pub addictions: Vec<Value>,
-    pub followers: Vec<Value>,
-    pub known_traps: Vec<Trap>,
+    pub addictions: Vec<Value>, // Need further research
+    pub followers: Vec<Value>,  // Need further research
     pub worn: Vec<Clothing>,
-    pub activity_vehicle_part_index: i32,
-    pub inv: Vec<Inventory>,
-    pub weapon: Option<Inventory>,
-    pub last_target_pos: Value,
-    pub faction_warnings: Vec<Value>,
-    pub ammo_location: HashMap<String, String>,
-    pub camps: Vec<Value>,
+    pub inv: Vec<Value>, // Need further research
+    pub weapon: Option<Value>, // Huge mess
+    pub last_target_pos: Value, // There is no null in Rustlang
+    pub destination_point: Value, // See above
+    pub faction_warnings: Vec<Value>, // Need further research
+    pub camps: Vec<Value>, // Need further research
     pub profession: String,
     pub scenario: String,
     pub controlling_vehicle: bool,
     pub grab_point: Vec<i32>,
     pub grab_type: String,
-    pub focus_pool: i32,
-    pub style_selected: String,
-    pub keep_hands_free: bool,
-    pub move_mode: String,
-    pub magic: Magic,
+    pub focus_pool: i64,
     pub str_upgrade: i32,
     pub dex_upgrade: i32,
     pub int_upgrade: i32,
     pub per_upgrade: i32,
-    pub activity: HashMap<String, String>,
-    pub backlog: Vec<Value>,
-    pub temp_cur: Vec<i32>,
-    pub temp_conv: Vec<i32>,
-    pub frostbite_timer: Vec<i32>,
     pub learned_recipes: Vec<String>,
     pub items_identified: Vec<String>,
-    pub vitamin_levels: HashMap<String, i32>,
-    pub stomach: GutContents,
-    pub guts: GutContents,
-    pub translocators: HashMap<String, Vec<Value>>,
-    pub morale: Vec<MoraleModifier>,
+    pub translocators: Value, // Need further research
     pub active_mission: i32,
-    pub active_missions: Vec<Value>,
-    pub completed_missions: Vec<Value>,
-    pub failed_missions: Vec<Value>,
+    pub active_missions: Vec<i32>,
+    pub completed_missions: Vec<i32>,
+    pub failed_missions: Vec<i32>,
     pub show_map_memory: bool,
-    pub assigned_invlet: Vec<Value>,
-    pub invcache: Vec<Value>
+    pub assigned_invlet: Vec<Value>, // Need further research
+    pub invcache: Vec<Value>, // Need further research
+    pub calorie_diary: Vec<Value>,  // Vec of hashes - spent, gained, activity(another vec)
+    pub preferred_aiming_mode: String
 }
 
-// A struct for player messages
+// Data about proficiencies
 #[derive(Serialize, Deserialize, Debug, Default)]
-pub struct PlayerMessages {
-    pub messages: Vec<Msg>,
-    pub curmes: i64
+pub struct ProficiencyData {
+    pub known: Vec<Value>, // I assume these two hash keys are the same data
+    pub learning: Vec<Value> // type but need further research
 }
 
-// A struct for messages themselves
+// Info about martial arts
 #[derive(Serialize, Deserialize, Debug, Default)]
-pub struct Msg {
-    pub turn: i64,
-    pub message: String,
-    pub count: i32,
-    #[serde(rename = "type")]
-    pub _type: i32
+pub struct MartialArts {
+    pub ma_styles: Vec<String>,
+    pub keep_hands_free: bool,
+    pub style_selected: String
 }
 
-// A struct for the player effects
+// Info about body part current status
 #[derive(Serialize, Deserialize, Debug, Default)]
-pub struct Effect {
-    pub eff_type: String,
-    pub duration: i32,
-    pub bp: i32,
-    pub permanent: bool,
-    pub intensity: i32,
-    pub start_turn: i64
+pub struct BodyInfo {
+    pub head: BodyPartStatus,
+    pub eyes: BodyPartStatus,
+    pub mouth: BodyPartStatus,
+    pub torso: BodyPartStatus,
+    pub arm_l: BodyPartStatus,
+    pub arm_r: BodyPartStatus,
+    pub hand_l: BodyPartStatus,
+    pub hand_r: BodyPartStatus,
+    pub leg_l: BodyPartStatus,
+    pub foot_l: BodyPartStatus, // Yes i know this isn't the same order as arm/hand but
+    pub leg_r: BodyPartStatus,  // this is how it appears in the save file
+    pub foot_r: BodyPartStatus
+}
+
+// Details of body info for each tracked body part
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct BodyPartStatus {
+    pub id: String,
+    pub hp_cur: i32,
+    pub hp_max: i32,
+    pub damage_bandaged: i32,
+    pub damage_disinfected: i32,
+    pub wetness: i32,
+    pub temp_cur: i32,
+    pub temp_conv: i32,
+    pub frostbite_timer: i32
 }
 
 // A struct for player mutations
@@ -389,15 +275,6 @@ pub struct Skill {
     pub highestlevel: i64
 }
 
-// A struct for known traps
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct Trap {
-    pub x: i32,
-    pub y: i32,
-    pub z: i32,
-    pub trap: String
-}
-
 // A struct for player bionics
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Bionic {
@@ -414,12 +291,11 @@ pub struct Bionic {
 pub struct Clothing {
     pub typeid: String,
     pub bday: Option<i32>,
-    pub damaged: Option<i32>,
-    pub last_rot_check: i32,
+    pub owner: String,
     pub last_temp_check: i32,
     pub item_tags: Option<Vec<String>>,
-    pub owner: String,
-    pub relic_data: Value
+    pub components: Option<Value>, // Big mess
+    pub contents: Option<Value> // Another big mess and also recursive
 }
 
 // A struct for player inventory with optional fields.  Can be
@@ -445,15 +321,22 @@ pub struct Inventory {
 // A struct for player magic
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Magic {
-    mana: i32,
-    spellbook: Vec<String>
+    pub mana: i32,
+    pub spellbook: Vec<Spell>,
+    pub invlets: Value  // Empty hash?
+}
+
+// Magic spells
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Spell {
+    pub id: String,
+    pub xp: i32
 }
 
 // A struct for stomach/guts
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct GutContents {
     vitamins: HashMap<String, i32>,
-    vitamins_absorbed: HashMap<String, i32>,
     calories: i32,
     water: String,
     max_volume: String,
@@ -461,7 +344,7 @@ pub struct GutContents {
     last_ate: i64
 }
 
-// A struct for player morale
+// A struct for player morale modifiers
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct MoraleModifier {
     #[serde(rename = "type")]
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,11 @@
-// Gui components
-use iced::{button, Application, Button, Column, Command, Element, Settings, Text};
-
-// And of course the CDDA stuff itself
+// It doesn't really do much right now, does it?
 use cdda_editor::CDDASave;
 
-// This thing's just a POC right now, so stuff is hard coded.
-// Don't judge me!
 fn main() { 
-    CDDAEditor::run(Settings::default())
+    let _loaded = match CDDASave::from_file("test.sav") {
+        Ok(x) => x,
+        Err(_e) => { panic!("Error loading save"); }
+    };
+    println!("It did a thing wow");
 }
 
-// A Struct to implement Application on that holds the GUI state
-#[derive(Default)]
-struct CDDAEditor {
-    savefile: CDDASave,
-    loaded: bool,
-    some_state: String,
-    a_button: button::State,
-}
-
-// An enum defining messages that can be passed from widgets
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    Pressed,
-}
-
-// Impl the Application trait
-impl Application for CDDAEditor {
-    type Message = Message;
-    
-    fn new() -> (Self, Command<Message>) {
-        (Self::default(), Command::none())
-    }
-    
-    fn title(&self) -> String {
-        String::from("CDDA Save File Editor")
-    }
-    
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::Pressed => {
-                if self.loaded == true { return Command::none(); }
-                let loaded = match CDDASave::from_file("test.sav") {
-                    Ok(x) => x,
-                    Err(_e) => { panic!("Error loading save"); }
-                };
-                self.savefile = loaded;
-                self.loaded = true;
-                self.some_state = "Save Loaded".to_string();
-            }
-        }
-        
-        Command::none()
-    }
-    
-    fn view(&mut self) -> Element<Message> {
-        Column::new()
-            .push(
-                Button::new(&mut self.a_button, Text::new("Load Save File"))
-                    .on_press(Message::Pressed),
-            )
-            .push(
-                Text::new(self.some_state.to_string()).size(50),
-            )
-            .into()
-    }
-}
